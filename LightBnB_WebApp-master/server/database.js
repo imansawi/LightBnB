24a85b5944fdd31d700e2fc8b11e843791b27245
imansawi <imansawi@hotmail.com>
2021-03-05 00:54:21 +0000

    Modified database.js updated addProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -160,7 +160,7 @@ const addProperty = function (property) {
   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, 
     cost_per_night, street, city, province, post_code, country, parking_spaces, 
     number_of_bathrooms, number_of_bedrooms)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING id`,
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`,
     [
       owner_id,
       title,
